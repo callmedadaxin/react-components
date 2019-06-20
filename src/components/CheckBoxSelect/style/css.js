7e73b61df51b5dcfec1beece6ae0bf3d22ab80ac
zoushijun <zoushijun@threatbook.cn>
2019-06-20 16:10:09 +0800

    /Users/zsj/work/zsjbin/git/git.sh

--- src/components/CheckBoxSelect/style/css.js
+++
@@ -1,7 +0,0 @@
-import "../../../styles/index.css";
-import "./index.css";
-import "../../Checkbox/style/css";
-import "../../Dropdown/style/css";
-import "../../Icon/style/css";
-import "../../Button/style/css";
-import "../../Input/style/css";
--- src/components/CheckBoxSelect/style/index.js
+++
@@ -1,7 +0,0 @@
-import "../../../styles/index.scss";
-import "./index.scss";
-import "../../Checkbox/style";
-import "../../Dropdown/style";
-import "../../Icon/style";
-import "../../Button/style";
-import "../../Input/style";
